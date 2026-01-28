6b3bdd477c20cef17cac250cbaee0ca081a65071
dionisos2 <denis.baudouin@gmail.com>
2026-01-28 22:38:44 +0100

    Small changes

--- examples/for_today.py
+++ examples/for_today.py
@@ -7,19 +7,21 @@ from datetime import date
 
 
 def get_today_todo(file_path):
-    base = PyOrgMode.OrgDataStructure()
-    base.load_from_file(file_path)
+	base = PyOrgMode.OrgDataStructure()
+	base.load_from_file(file_path)
 
-    nb_task_missed = 0
-    nb_task_for_today = 0
+	nb_task_missed = 0
+	nb_task_for_today = 0
 
-    for task in base.extract_todo_list():
-        print(task)
-        if hasattr(task, "scheduled") and task.scheduled and task.scheduled.should_be_done():
-            if task.scheduled.should_be_done_today():
-                nb_task_for_today += 1
-            else:
-                nb_task_missed += 1
+	for task in base.extract_todo_list():
+		print(task)
+		if hasattr(task, "scheduled") and task.scheduled and task.scheduled.should_be_done():
+			if task.scheduled.should_be_done_today():
+				print("--- for today")
+				nb_task_for_today += 1
+			else:
+				print("--- for yesterday")
+				nb_task_missed += 1
 
-    return (nb_task_for_today, nb_task_missed)
+	return (nb_task_for_today, nb_task_missed)
 
